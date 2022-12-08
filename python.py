50c1b87bb1187c95e69cbd2b224804ea4788cef5
yuuiz <104612556+yuuiz@users.noreply.github.com>
2022-12-08 04:09:48 +0000

    fixed python lol

--- python.py
+++ python.py
@@ -1,15 +1,26 @@
 import json
+def is_json_key_present(json, key):
+    try:
+        buf = json[key]
+    except KeyError:
+        return False
 
-banned = 0
+    return True
 def main():
+	print('version 2.01')
 	id = int(input('id>'))
 	reason = input('reason>')
 	f1 = open('users.json').read()
 	with open('users.json', 'w') as f:
 		j = json.loads(f1)
-		j[id] = reason
-		if j[id] == reason:
-			print('updated json')
-		f.write(json.dumps(j))
+		if is_json_key_present(j, id):
+			j[id] = reason
+			if j[id] == reason:
+				print('updated json')
+			f.write(json.dumps(j))
+		else:
+			print('already existed')
+			f.write(f1)
+		
 	main()
 main()
